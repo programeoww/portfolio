b19086fdea6a1826ec45abdd5ebc017e318e64e4
hieunm156 <hieunm156@gmail.com>
2023-10-24 16:04:11 +0700

    update meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,45 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Helmet } from "react-helmet";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Helmet
+        htmlAttributes={{ lang: "en" }}
+        title="Portfolio | @programeoww"
+        meta={[
+          {
+            name: "description",
+            content: "Portfolio of Hieu Ngo Minh",
+          },
+          {
+            name: "keywords",
+            content: "Portfolio, Hieu Ngo Minh, programeoww",
+          },
+          {
+            name: "author",
+            content: "@programeoww",
+          },
+          {
+            name: "og:title",
+            content: "Portfolio | @programeoww",
+          },
+          {
+            name: "og:description",
+            content: "Portfolio of Hieu Ngo Minh",
+          },
+          {
+            name: "og:type",
+            content: "website",
+          },
+          {
+            name: "og:url",
+            content: "https://programeoww.github.io/",
+          },
+        ]}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
